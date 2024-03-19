5934da21265751d89debc4ab41b50e94ace4cf53
praveen200307 <crouselearn@gmail.com>
2024-03-19 03:44:25 -0400

    feat:added email functionality

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,20 +1,58 @@
-// function sendMail(e){
+const validate = new window.JustValidate('#contactForm');
 
-//     e.preventDefault();
-//     var params ={
-//         from_name:document.getElementById("fullName").value,
-//         email_id:document.getElementById("email_id").value,
-//         message:document.getElementById("message").value
-//     }
-//      emailjs.send("service_lgq53e8","template_6az87sr",params).then(function(res){
-//         alert("You are submit Success");
-//         document.forms.emailForm.reset();
-//     })
-// }
+validate
+    .addField('#fullName', [
+        {
+            rule: 'required',
+            errorMessage: "name is required",
+        },
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+        {
+            rule: 'maxLength',
+            value: 15,
+        },
+    ]).addField('#email_id', [
+        {
+            rule: 'required',
+            errorMessage: "email is required",
+        },
+        {
+            rule: 'email',
+        },
+    ]).addField('#message', [
+        {
+            rule: 'required',
+            errorMessage: "message is required",
+        },
+    ])
 
-// const submitBtn=document.getElementById('sendBtn');
 
-// submitBtn.addEventListener('click',sendMail);
+validate.onSuccess((event) => {
+        event.preventDefault();
+        var params = {
+            from_name: document.getElementById("fullName").value,
+            email_id: document.getElementById("email_id").value,
+            message: document.getElementById("message").value
+        }
+        emailjs.send("service_lgq53e8", "template_6az87sr", params).then(function(res) {
+            const popupEl=document.querySelector('.popup')
+            popupEl.style.display="flex"
+            document.forms.emailForm.reset();
+        })
+});
 
 
-// pratice make a man perfect
+const popupCloseBtn =document.querySelector('#closeBtn');
+
+popupCloseBtn.addEventListener('click',()=>{
+    const popupEl=document.querySelector('.popup')
+    popupEl.style.display="none"
+})
+
+document.addEventListener('click',()=>{
+    const popupEl=document.querySelector('.popup')
+    popupEl.style.display="none"
+})
