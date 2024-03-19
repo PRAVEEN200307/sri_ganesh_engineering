07c3ca87fb02e1666ca78e951e46a740c419f96c
praveen200307 <crouselearn@gmail.com>
2024-03-19 10:58:07 -0400

    feat: added the link from footer

--- assets/js/email.js
+++ assets/js/email.js
@@ -37,7 +37,7 @@ validate.onSuccess((event) => {
             email_id: document.getElementById("email_id").value,
             message: document.getElementById("message").value
         }
-        emailjs.send("service_lgq53e8", "template_6az87sr", params).then(function(res) {
+        emailjs.send("service_ykjs8ga", "template_6az87sr", params).then(function(res) {
             const popupEl=document.querySelector('.popup')
             popupEl.style.display="flex"
             document.forms.emailForm.reset();
