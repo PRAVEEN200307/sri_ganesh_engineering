866828d5a8d7890dbe5a91223ede7bcdd4a2b237
praveen200307 <crouselearn@gmail.com>
2024-03-19 02:43:38 -0400

    feat: added the poup message

---
+++ assets/js/email.js
@@ -0,0 +1,20 @@
+// function sendMail(e){
+
+//     e.preventDefault();
+//     var params ={
+//         from_name:document.getElementById("fullName").value,
+//         email_id:document.getElementById("email_id").value,
+//         message:document.getElementById("message").value
+//     }
+//      emailjs.send("service_lgq53e8","template_6az87sr",params).then(function(res){
+//         alert("You are submit Success");
+//         document.forms.emailForm.reset();
+//     })
+// }
+
+// const submitBtn=document.getElementById('sendBtn');
+
+// submitBtn.addEventListener('click',sendMail);
+
+
+// pratice make a man perfect
